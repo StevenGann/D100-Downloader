a53d664cee68464a5cce15a1eefbcf41d445e737
Steven Gann <sgann2012@fau.edu>
2018-07-10 21:22:08 -0700

    Improved HTML template, General cleanup

--- D100Downloader/Program.cs
+++ D100Downloader/Program.cs
@@ -1,22 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace D100Downloader
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             string template = System.IO.File.ReadAllText(@"template.html");
-            string html = DndSpeakClient.DownloadHtml(@"http://dndspeak.com/2018/06/100-royal-family-drama/");
+            string url = @"http://dndspeak.com/2018/06/100-royal-family-drama/";
+            string html = DndSpeakClient.DownloadHtml(url);
             string title = DndSpeakClient.ExtractTitle(html);
             string subtitle = DndSpeakClient.ExtractSubtitle(html);
             List<string> items = DndSpeakClient.ExtractList(html);
-            string tsv = DndSpeakClient.ListToTsv(items, title, subtitle);
-            string newhtml = DndSpeakClient.ListToHtml(items, title, subtitle, template);
+            string tsv = DndSpeakClient.ListToTsv(items, title, subtitle, url);
+            string newhtml = DndSpeakClient.ListToHtml(items, title, subtitle, url, template);
             Console.WriteLine(title);
             Console.WriteLine(subtitle);
             DndSpeakClient.StringToFile(tsv, DndSpeakClient.MakeValidFileName(title) + ".tsv");
